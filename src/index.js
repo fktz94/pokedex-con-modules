bfcf765fc85842f47ef3c7559c91333368c76177
fktz94 <106633973+fktz94@users.noreply.github.com>
2023-02-06 16:35:44 -0300

    funcionalidad ir adelante/atras

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import {
   funcionalizarBotones,
 } from './ui.js';
 
-async function actualizar() {}
+async function actualizar(url) {
+  const respuesta = await llamarListaPokemones(url);
+  await listarPokemones(respuesta.results);
+  asignarDireccionesALosBotones(respuesta.previous, respuesta.next);
+  funcionalizarBotones(actualizar);
+}
 
 async function cargar() {
   const respuesta = await llamarListaPokemones();
   await listarPokemones(respuesta.results);
   asignarDireccionesALosBotones(respuesta.previous, respuesta.next);
-  funcionalizarBotones(llamarListaPokemones());
+  funcionalizarBotones(actualizar);
 }
 cargar();
